2eca65a50c870be57312c70b610391d0b302d0d4
LeoLuZhang <m18729209100@163.com>
2018-10-13 17:12:52 +0800

    State

---
+++ src/State/StartState.java
@@ -0,0 +1,13 @@
+package State;
+
+public class StartState implements State{
+    @Override
+    public void doAction(Context context) {
+        System.out.println("Player is in start state");
+    }
+
+    @Override
+    public String toString(){
+        return "Start State";
+    }
+}
